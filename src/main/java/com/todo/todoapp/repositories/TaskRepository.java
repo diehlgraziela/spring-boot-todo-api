37b026c31b133b09dd50e48a4450e9554a394b14
Graziela Diehl <gdiehl@imply.com>
2024-10-20 18:11:16 -0300

    Creating User and Task repositories

---
+++ src/main/java/com/todo/todoapp/repositories/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.todo.todoapp.repositories;
+
+import com.todo.todoapp.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUser_Id(Long id);
+
+    //@Query(value = "SELECT t FROM Task t WHERE t.user.id = :user_id")
+    //List<Task> findByUser_Id(@Param("user_id") Long id);
+
+//    @Query(value = "SELECT * FROM task t WHERE t.user_id = :user_id", nativeQuery = true)
+//    List<Task> findByUser_Id(@Param("user_id") Long id);
+}
