c47a98fed93ebd2c1fd5abaa0c3f367a38db6ea5
Graziela Diehl <gdiehl@imply.com>
2024-10-23 20:36:59 -0300

    Creating User controller

---
+++ src/main/java/com/todo/todoapp/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.todo.todoapp.controllers;
+
+import com.todo.todoapp.models.User;
+import com.todo.todoapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = this.userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody User user) {
+        this.userService.create(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody User user, @PathVariable Long id) {
+        user.setId(id);
+        user = this.userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/todo/todoapp/models/User.java
+++ src/main/java/com/todo/todoapp/models/User.java
@@ -1,5 +1,6 @@
 package com.todo.todoapp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
@@ -61,6 +62,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return tasks;
     }
