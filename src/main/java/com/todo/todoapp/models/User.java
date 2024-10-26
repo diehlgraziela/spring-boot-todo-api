a739b35feecc1b32e4d3d9adac0bae0854a759f1
Graziela Diehl <gdiehl@imply.com>
2024-10-26 20:09:01 -0300

    Adding validation dependency to project

--- src/main/java/com/todo/todoapp/models/User.java
+++ src/main/java/com/todo/todoapp/models/User.java
@@ -2,7 +2,10 @@ package com.todo.todoapp.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +21,16 @@ public class User {
     @Column(name = "id", unique = true)
     private Long id;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "username", length = 100, nullable = false, unique = true)
+    @Size(min = 2, max = 100)
+    @NotBlank
     private String username;
 
+    @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
+    @Size(min = 8, max = 60)
+    @NotBlank
     private String password;
 
     // Um user pode ter várias tasks, One -> user / Many -> tasks
